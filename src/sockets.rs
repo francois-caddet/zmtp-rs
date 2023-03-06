6983c615f14b8621764a01f32b27ff767a97f048
François Caddet <francois.caddet@icloud.com>
2023-03-06 19:06:02 +0100

    Document `sockets` module

--- src/sockets.rs
+++ src/sockets.rs
@@ -1,9 +1,27 @@
+//! Zmtp provided sockets (base, plain password, curve)
 use crate::{errors::ConnectionError, Result};
 use tokio::net::TcpStream;
 
+/// The base ZMTP socket.
+///
+/// Does not provide any authentication/encryption security.
 pub struct Zmtp(TcpStream);
 
 impl Zmtp {
+    /// Connect to `tcp://host:port`.
+    ///
+    /// Only provide tcp base transport for now. But will become generic over the base transport
+    /// before `v0.0.2`
+    ///
+    /// # Exemple
+    ///
+    /// ```rust
+    /// use zmtp::sockets;
+    ///
+    /// let port = 55555;
+    /// let host = "localhost";
+    /// sockets::Zmtp::connect(host, port);
+    /// ```
     pub async fn connect(host: &str, port: u16) -> Result<Self> {
         TcpStream::connect((host, port))
             .await
