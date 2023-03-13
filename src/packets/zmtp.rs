b8a439bff09f0cf8123cba3d2000166ec9e645cc
François Caddet <francois.caddet@icloud.com>
2023-03-13 20:04:52 +0100

    Sort of state machin for the connection process.
    - add dependency to futures.rs
    - the state machine implementation go until the full greeting is send and the remote one is verified.
    - Already add some code to parse and construct zmtp frames

--- src/packets/zmtp.rs
+++ src/packets/zmtp.rs
@@ -1,29 +1,132 @@
-#[repr(C)]
+#[repr(C, packed)]
+#[derive(Debug)]
 pub struct Greeting {
     signature: [u8; 10],
     version: Version,
-    mechanism: [char; 20],
+    mechanism: Mechanism,
     as_server: u8,
     filler: [u8; 31],
 }
 
-#[repr(C)]
+#[repr(C, packed)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct Version {
     pub major: u8,
     pub minor: u8,
 }
 
+const fn zerro_padded<const M: usize, const N: usize>(arr: &[u8; M]) -> [u8; N] {
+    let mut ret = [0u8; N];
+    let mut i = 0;
+    while i < M {
+        ret[i] = arr[i];
+        i += 1;
+    }
+    ret
+}
+
+#[repr(C, packed)]
+#[derive(Debug, PartialEq, Eq)]
+pub struct Mechanism(pub [u8; 20]);
+
+impl Mechanism {
+    pub const NULL: Self = Self(zerro_padded(br#"NULL"#));
+}
+
+#[derive(Debug)]
+pub struct Frame<'a> {
+    flag: u8,
+    size: u64,
+    body: &'a [u8],
+}
+
+struct Flag(u8);
+
+impl Flag {
+    const TYPE_MASK: Self = Self(0b00000100);
+    const SIZE_MASK: Self = Self(0b00000010);
+    const LAST: Self = Self(0b00000001);
+
+    pub fn command(self) -> Self {
+        // set bit 2 to 1 and keep the 2 others value
+        Self(self.0 | 0b00000100)
+    }
+
+    pub fn message(self) -> Self {
+        // set bit 2 to 0 and keep the 2 others value
+        Self(self.0 & 0b00000011)
+    }
+
+    pub fn big(self) -> Self {
+        // set bit 1 to 1 and keep the 2 others value
+        Self(self.0 | 0b00000010)
+    }
+
+    pub fn small(self) -> Self {
+        // set bit 1 to 0 and keep the 2 others value
+        Self(self.0 & 0b00000101)
+    }
+
+    pub fn last(self) -> Self {
+        // set bit 0 to 1 and keep the 2 others value
+        Self(self.0 | 0b00000001)
+    }
+
+    pub fn frame_remaining(self) -> Self {
+        // set bit 0 to 0 and keep the 2 others value
+        Self(self.0 & 0b00000110)
+    }
+
+    pub fn is_command(&self) -> bool {
+        self.0 & Self::TYPE_MASK.0 > 0
+    }
+
+    pub fn is_message(&self) -> bool {
+        self.0 & Self::TYPE_MASK.0 == 0
+    }
+
+    pub fn is_big(self) -> bool {
+        self.0 & Self::SIZE_MASK.0 > 0
+    }
+
+    pub fn is_last(self) -> bool {
+        self.0 & Self::LAST.0 > 0
+    }
+}
+
 impl Greeting {
-    const SIG: [u8; 10] = [0xff, 0, 0, 0, 0, 0, 0, 0, 0, 0xf7];
+    const SIG: [u8; 10] = [0xff, 0, 0, 0, 0, 0, 0, 0, 0, 0x7f];
     const FILLER: [u8; 31] = [0x00; 31];
 
     pub fn new() -> Self {
         Self {
             signature: Self::SIG,
             version: Version { major: 3, minor: 0 },
-            mechanism: [0 as char; 20],
+            mechanism: Mechanism::NULL,
             as_server: 0u8,
             filler: Self::FILLER,
         }
+    }
+
+    pub fn with_mechanism(mut self, mechanism: Mechanism) -> Self {
+        self.mechanism = mechanism;
+        self
+    }
+}
+
+pub(crate) trait Packet: Sized {
+    fn as_bytes(&self) -> &[u8];
+    fn from_bytes(buf: &[u8]) -> &Self;
+}
+
+impl Packet for Greeting {
+    fn as_bytes<'a>(&'a self) -> &'a [u8] {
+        unsafe { ::core::slice::from_raw_parts((self as *const Self) as *const u8, 64) }
+    }
+
+    fn from_bytes<'a>(buf: &'a [u8]) -> &'a Self {
+        let (head, body, _tail) = unsafe { buf.align_to::<Self>() };
+        assert!(head.is_empty(), "Data was not aligned");
+        &body[0]
     }
 }
--- src/sockets.rs
+++ src/sockets.rs
@@ -1,11 +1,15 @@
 //! Zmtp provided sockets (base, plain password, curve)
+use crate::packets::{Greeting, Packet};
 use crate::{errors::ConnectionError, Result};
+
+use futures::TryFutureExt;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
 
 /// The base ZMTP socket.
 ///
 /// Does not provide any authentication/encryption security.
-pub struct Zmtp(TcpStream);
+pub struct Zmtp(states::AgreedMechanism);
 
 impl Zmtp {
     /// Connect to `tcp://host:port`.
@@ -23,9 +27,90 @@ impl Zmtp {
     /// sockets::Zmtp::connect(host, port);
     /// ```
     pub async fn connect(host: &str, port: u16) -> Result<Self> {
-        TcpStream::connect((host, port))
+        states::Root::connect(host, port)
+            .and_then(|c| c.version(3, 0))
+            .and_then(|c| c.mechanism(crate::packets::Mechanism::NULL))
+            .map_ok(Self)
+            .err_into()
             .await
-            .map_err(|_| ConnectionError::UnaccessibleHost(host.to_string(), port).into())
-            .map(Self)
     }
+
+    pub fn version(&self) -> crate::packets::Version {
+        crate::packets::Version { major: 3, minor: 0 }
+    }
+}
+
+mod states {
+    use crate::errors::ConnectionError;
+    use crate::packets::{Greeting, Packet};
+    use futures::{FutureExt, TryFutureExt};
+    use tokio::io::{split, AsyncReadExt, AsyncWriteExt};
+    use tokio::net::TcpStream;
+
+    pub struct Root;
+    impl Root {
+        pub async fn connect(host: &str, port: u16) -> Result<Connected, ConnectionError> {
+            TcpStream::connect((host, port))
+                .map_ok(Connected)
+                .map_err(|_| ConnectionError::UnaccessibleHost(host.to_string(), port).into())
+                .await
+        }
+    }
+
+    pub struct Connected(TcpStream);
+    impl Connected {
+        pub async fn version(mut self, major: u8, minor: u8) -> Result<Versioned, ConnectionError> {
+            if (major, minor) != (3u8, 0u8) {
+                return Err(ConnectionError::VersionMismatch());
+            }
+            let greeting = Greeting::new();
+            let (mut reader, mut writer) = split(self.0);
+            tokio::try_join![
+                async {
+                    let mut buf = [0u8; 11];
+                    reader.read_exact(&mut buf).await?;
+                    match buf {
+                        [0xff, _, _, _, _, _, _, _, _, last, v] if (last & 0x01 > 0) && v >= 3 => {
+                            Ok(())
+                        }
+                        _ => Err(ConnectionError::VersionMismatch()),
+                    }
+                },
+                writer.write_all(&greeting.as_bytes()[..11]).err_into(),
+            ]?;
+            Ok(Versioned(reader.unsplit(writer), greeting))
+        }
+    }
+
+    pub struct Versioned(TcpStream, Greeting);
+    impl Versioned {
+        pub async fn mechanism(
+            self,
+            m: crate::packets::Mechanism,
+        ) -> Result<AgreedMechanism, ConnectionError> {
+            use crate::packets::Mechanism;
+            let (mut reader, mut writer) = split(self.0);
+            tokio::try_join![
+                async {
+                    let mut remote_m = [0u8; 20];
+                    // ignore the 12th byte. it represent the minor version in ZMTP 3.0
+                    reader.read_exact(&mut [0u8; 1]).await?;
+                    reader.read_exact(&mut remote_m).await?;
+                    if m == Mechanism(remote_m) {
+                        reader
+                            .read_exact(&mut [0u8; 31])
+                            .map_ok(|_| ())
+                            .err_into()
+                            .await
+                    } else {
+                        Err(ConnectionError::MechanismMismatch())
+                    }
+                },
+                writer.write_all(&self.1.as_bytes()[11..]).err_into(),
+            ]?;
+            Ok(AgreedMechanism(reader.unsplit(writer)))
+        }
+    }
+
+    pub struct AgreedMechanism(TcpStream);
 }
