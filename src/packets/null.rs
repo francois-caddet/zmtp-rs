0bd963f8a64ca85dd08cea0613645860e078a4a1
François Caddet <francois.caddet@icloud.com>
2023-03-18 19:14:03 +0100

    Reverse transform from `Command` to `Vec<u8>`
    Also added unit test for this feature

--- src/packets/null.rs
+++ src/packets/null.rs
@@ -1,6 +1,6 @@
 use super::zmtp::RawFrame;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum Command {
     Error(String),
     Ready {
@@ -9,7 +9,7 @@ pub enum Command {
     },
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub enum Frame {
     Command(Command),
 }
@@ -63,5 +63,53 @@ impl TryFrom<RawFrame> for Frame {
             }
             RawFrame::Message(_) => todo!(),
         })
+    }
+}
+
+impl Command {
+    pub fn to_vec_u8(&self) -> Vec<u8> {
+        match self {
+            Command::Ready {
+                socket_type,
+                identity,
+            } => {
+                let key = br#"READY"#;
+                let mut buf = Vec::new();
+                buf.push(key.len() as u8);
+                buf.extend(key);
+                let key = br#"Socket-Type"#;
+                buf.push(key.len() as u8);
+                buf.extend(key);
+                buf.extend(&(socket_type.len() as u32).to_be_bytes());
+                buf.extend(socket_type);
+                if let Some(identity) = identity {
+                    let key = br#"Identity"#;
+                    buf.push(key.len() as u8);
+                    buf.extend(key);
+                    buf.extend(&(identity.len() as u32).to_be_bytes());
+                    buf.extend(identity);
+                }
+                buf
+            }
+            Command::Error(_msg) => todo!(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Command, Frame};
+    use crate::packets;
+
+    #[test]
+    fn simetric_serialize() {
+        let cmd = Command::Ready {
+            socket_type: Vec::from(&b"REQ"[..]),
+            identity: Some(Vec::from(&b"test.identity"[..])),
+        };
+        assert_eq!(
+            Frame::try_from(packets::RawFrame::Command(cmd.to_vec_u8())),
+            Ok(Frame::Command(cmd))
+        );
     }
 }
